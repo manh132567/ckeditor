5791558b8ddee5b17a988562733373f4ce9dae61
Cong Nguyen <nguyencongt3@gmail.com>
2022-08-30 11:21:21 +0700

    Add image resize

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -11,12 +11,14 @@ import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials.js";
 import Heading from "@ckeditor/ckeditor5-heading/src/heading.js";
 import Image from "@ckeditor/ckeditor5-image/src/image.js";
 import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption.js";
+import ImageResize from "@ckeditor/ckeditor5-image/src/imageresize.js";
 import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle.js";
 import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar.js";
 import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload.js";
 import Indent from "@ckeditor/ckeditor5-indent/src/indent.js";
 import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic.js";
 import Link from "@ckeditor/ckeditor5-link/src/link.js";
+import LinkImage from "@ckeditor/ckeditor5-link/src/linkimage.js";
 import List from "@ckeditor/ckeditor5-list/src/list.js";
 import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed.js";
 import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph.js";
@@ -38,12 +40,14 @@ Editor.builtinPlugins = [
   Heading,
   Image,
   ImageCaption,
+  ImageResize,
   ImageStyle,
   ImageToolbar,
   ImageUpload,
   Indent,
   Italic,
   Link,
+  LinkImage,
   List,
   MediaEmbed,
   Paragraph,
@@ -85,6 +89,7 @@ Editor.defaultConfig = {
       "imageStyle:inline",
       "imageStyle:block",
       "imageStyle:side",
+      "linkImage",
     ],
   },
   table: {
